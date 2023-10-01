92a1a68cb7ceeb5bc5e13d087d75a73768e61964
Cradling7103 <56605882+Cradling@users.noreply.github.com>
2023-10-01 15:28:27 +0800

    Update Twitter-AutoScroll.user.js

--- UserScript/Twitter-AutoScroll.user.js
+++ UserScript/Twitter-AutoScroll.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Twitter AutoScroll
-// @namespace    your-namespace
+// @namespace    https://github.com/Cradling/Backup
 // @version      1.0
 // @description  自动往下滑动Twitter页面
 // @author       Unknown
@@ -11,142 +11,158 @@
 (function () {
   'use strict';
 
-  // 创建一个标志变量来控制自动滑动的开关
-  let autoScrollEnabled = false;
-  let intervalId;
-
   // 创建一个容器元素
   const container = document.createElement('div');
   container.id = 'auto-scroll-container';
 
-  // 创建一个按钮元素
-  const toggleButton = document.createElement('button');
-  toggleButton.textContent = '滑动开关';
-  toggleButton.id = 'auto-scroll-toggle-button';
+  // 创建一个往上滑按钮
+  const scrollUpButton = document.createElement('button');
+  scrollUpButton.textContent = 'Up';
+  scrollUpButton.id = 'auto-scroll-up-button';
 
-  // 创建一个滚动距离输入框
-  const distanceInput = document.createElement('input');
-  distanceInput.type = 'number';
-  distanceInput.id = 'auto-scroll-distance-input';
-  distanceInput.placeholder = '距离px';
+  // 创建一个往下滑按钮
+  const scrollDownButton = document.createElement('button');
+  scrollDownButton.textContent = 'Down';
+  scrollDownButton.id = 'auto-scroll-down-button';
 
-  // 创建一个时间间隔输入框
-  const intervalInput = document.createElement('input');
-  intervalInput.type = 'number';
-  intervalInput.id = 'auto-scroll-interval-input';
-  intervalInput.placeholder = '间隔ms';
+  // 创建一个停止按钮
+  const stopButton = document.createElement('button');
+  stopButton.textContent = 'Stop';
+  stopButton.id = 'auto-scroll-stop-button';
 
-  // 将按钮和输入框添加到容器中
-  container.appendChild(toggleButton);
-  container.appendChild(distanceInput);
-  container.appendChild(intervalInput);
+  // 将按钮添加到容器中
+  container.appendChild(scrollUpButton);
+  container.appendChild(scrollDownButton);
+  container.appendChild(stopButton);
 
   // 将容器添加到页面中
   document.body.appendChild(container);
 
-  // 按钮点击事件处理程序
-  toggleButton.addEventListener('click', function () {
-    // 切换自动滑动的状态
-    autoScrollEnabled = !autoScrollEnabled;
+  let intervalId;
 
-    // 如果开启自动滑动，则开始滑动页面
-    if (autoScrollEnabled) {
-      const distance = parseInt(distanceInput.value) || window.innerHeight;
-      const interval = parseInt(intervalInput.value) || 1000;
-      intervalId = setInterval(() => scrollPage(distance), interval);
-      toggleButton.textContent = '停止滑动';
-    } else {
-      // 如果关闭自动滑动，则停止滑动并更新按钮文本
-      clearInterval(intervalId);
-      toggleButton.textContent = '自动滑动开关';
-    }
+  // 按钮点击事件处理程序 - 往上滑
+  scrollUpButton.addEventListener('click', function () {
+    const distance = -2000; // 设置快速滑动的距离为负值，表示往上滑
+    const interval = 100; // 设置快速滑动的时间间隔
+    intervalId = setInterval(() => scrollPage(distance), interval);
+  });
+
+  // 按钮点击事件处理程序 - 往下滑
+  scrollDownButton.addEventListener('click', function () {
+    const distance = 2000; // 设置快速滑动的距离为正值，表示往下滑
+    const interval = 100; // 设置快速滑动的时间间隔
+    intervalId = setInterval(() => scrollPage(distance), interval);
+  });
+
+  // 按钮点击事件处理程序 - 停止滑动
+  stopButton.addEventListener('click', function () {
+    clearInterval(intervalId);
   });
 
   // 滑动页面的函数
   function scrollPage(distance) {
-    window.scrollBy(0, distance);
+    window.scrollBy({
+      top: distance,
+      behavior: 'auto' // 关闭动画效果
+    });
   }
 
-  // 添加样式以美化按钮和输入框，并设置容器的样式
+  // 添加样式以美化按钮和容器
   GM_addStyle(`
     #auto-scroll-container {
       position: fixed;
-      bottom: 10px;
+      top: 50%;
       left: 10px;
+      transform: translateY(-50%);
       z-index: 9999;
       display: flex;
-      align-items: center;
+      flex-direction: column;
+      align-items: flex-start;
+      opacity: 0;
+      transition: opacity 0.3s ease-in-out;
     }
 
-    #auto-scroll-toggle-button {
+    button {
       padding: 8px 12px;
       background-color: #1da1f2;
       border: none;
-      color: #fff;
+      color: #ffffff;
       font-size: 14px;
+      margin-bottom: 10px;
       cursor: pointer;
-      border-radius: 5px;
-      margin-right: 10px;
-      transition: background-color 0.3s ease;
+      border-radius: 4px;
+      opacity: 0;
+      transition: opacity 0.3s ease-in-out;
     }
 
-    #auto-scroll-toggle-button:hover {
-      background-color: #0c87b8;
+    /* 淡入淡出效果 */
+    #auto-scroll-container.fade-in {
+      opacity: 1;
     }
 
-    #auto-scroll-distance-input,
-    #auto-scroll-interval-input {
-      width: 80px;
-      padding: 4px;
-      border: 1px solid #ddd;
-      border-radius: 5px;
-      margin-right: 10px;
-      transition: border-color 0.3s ease;
+    #auto-scroll-container.fade-out {
+      opacity: 0;
     }
 
-    #auto-scroll-distance-input:focus,
-    #auto-scroll-interval-input:focus {
-      outline: none;
-      border-color: #1da1f2;
+    button.fade-in {
+      opacity: 1;
     }
 
-    @media screen and (max-width: 600px) {
+    button.fade-out {
+      opacity: 0;
+    }
+
+    /* 响应式设计 */
+    @media (max-width: 600px) {
       #auto-scroll-container {
-        flex-direction: column;
-        left: 5px;
-        bottom: 5px;
+        bottom: 10px;
+        top: auto;
+        left: auto;
+        right: 10px;
+        transform: none;
+        flex-direction: row;
+        align-items: center;
       }
 
-      #auto-scroll-toggle-button {
-        margin-bottom: 5px;
+      button {
+        margin-right: 10px;
       }
+    }
 
-      #auto-scroll-distance-input,
-      #auto-scroll-interval-input {
-        width: 100%;
-        margin-bottom: 5px;
-      }
+    /* 主题颜色 */
+    body[data-theme="dark"] #auto-scroll-container {
+      background-color: #000000;
+    }
+
+    body[data-theme="dark"] #auto-scroll-up-button,
+    body[data-theme="dark"] #auto-scroll-down-button {
+      background-color: #ffffff;
+    }
+
+    /* 可访问性优化 */
+    button:focus {
+      outline: none;
+      box-shadow: 0 0 0 3px rgba(29, 161, 242, 0.5);
     }
   `);
 
-  // 添加键盘事件处理程序，以便使用空格键切换自动滑动的开关
-  document.addEventListener('keydown', function (event) {
-    if (event.code === 'Space') {
-      event.preventDefault();
-      toggleButton.click();
-    }
-  });
+  // 淡入容器和按钮
+  setTimeout(() => {
+    container.classList.add('fade-in');
+    scrollUpButton.classList.add('fade-in');
+    scrollDownButton.classList.add('fade-in');
+    stopButton.classList.add('fade-in');
+  }, 1000);
 
-  // 添加可访问性属性
-  toggleButton.setAttribute('aria-pressed', 'false');
-  toggleButton.setAttribute('role', 'button');
-  toggleButton.setAttribute('aria-label', '切换自动滑动开关');
-
-  distanceInput.setAttribute('aria-label', '距离px）');
-  distanceInput.setAttribute('aria-describedby', 'auto-scroll-distance-input');
-  distanceInput.setAttribute('role', 'textbox');
-
-  intervalInput.setAttribute('aria-label', '间隔ms');
-  intervalInput.setAttribute('aria-describedby', 'auto-scroll-interval-input');
-  intervalInput.setAttribute('role', 'textbox');
+  // 淡出容器和按钮
+  setTimeout(() => {
+    container.classList.remove('fade-in');
+    container.classList.add('fade-out');
+    scrollUpButton.classList.remove('fade-in');
+    scrollUpButton.classList.add('fade-out');
+    scrollDownButton.classList.remove('fade-in');
+    scrollDownButton.classList.add('fade-out');
+    stopButton.classList.remove('fade-in');
+    stopButton.classList.add('fade-out');
+  }, 3000);
 })();
